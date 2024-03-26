108cb8dafe4d396a44c2609e7ba6cbdebcd3f93c
Medorpond <stcwh@naver.com>
2024-03-26 14:56:30 +0900

    Unit class Added
    -

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -3,8 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+public abstract class Unit : MonoBehaviour
 {
+    // Properties ::
+    [Header("Properties")]
+    [SerializeField]
+    private float maxHP;
+    private float currentHP;
+    private float moveSpeed;
+    private float movePoint;
+    private float range;
+    private float damage;
+    // :: Properties
     // Start is called before the first frame update
     void Start()
     {
@@ -16,4 +26,36 @@ public class Unit : MonoBehaviour
     {
         
     }
+
+    // Methods ::
+    // Battle ::
+    public void Attack(Unit _opponent)
+    {
+        _opponent.isDamaged(damage);
+    }
+    public void isDamaged(float _damage)
+    {
+        if(_damage >= currentHP)
+        {
+            currentHP = 0f;
+            isDead();
+        }
+        else { currentHP -= _damage; }
+    } //Damaging process
+
+    public void isDead()
+    {
+        Debug.Log(this.name + " is Dead!");
+    }
+    // :: Battle
+
+    // Move ::
+    public void Move(float _destX, float _destY)
+    {
+        // get Current Location, get destination's Location, subtract, move.
+        Debug.Log(this.name + " Moved to " + _destX + ", " + _destY);
+    }
+    // :: Move
+
+    // :: Methods
 }
