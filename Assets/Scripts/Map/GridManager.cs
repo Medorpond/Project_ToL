eabc1a9c1e719ff10752907a685584aa8d6c1f27
jyjiwoo <thswldn0317@kau.kr>
2024-04-03 03:27:40 +0900

    Random Map

--- Assets/Scripts/Map/GridManager.cs
+++ Assets/Scripts/Map/GridManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -29,14 +30,30 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
+                // 타일 생성
                 MapTile spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity, gameObject.transform);
                 spawnedTile.name = $"Tile({x}, {y})";
 
-                bool chooseColor = (x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0);
-                spawnedTile.SetTileColor(chooseColor);
+                // 타일의 타입 결정
+                int randomNum = Random.Range(1, 10);    // 랜덤 요소가 몇개 생성되는지 랜덤으로(변경예정)
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1) spawnedTile.tileType = TileType.block;
+                else
+                {
+                    spawnedTile.tileType = TileType.grass;
+
+                    // 랜덤으로 벽 생성
+                    for (int i = 0; i < randomNum; i++)
+                    {
+                        if (x == Random.Range(1, width) && y == Random.Range(1, height)) spawnedTile.tileType = TileType.wall;
+                    }
+                }
+
+                // 타입에 맞게 스프라이트 변경
+                spawnedTile.SettingTile(spawnedTile.tileType);
             }
         }
 
+        // 맵에 맞춰 카메라 조정
         camTransform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
     }
 }
--- Assets/Scripts/Map/MapTile.cs
+++ Assets/Scripts/Map/MapTile.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public enum TileType
 {
-    idle = 0, obstacle
+    grass, wall, block,
 }
 
 public class MapTile : MonoBehaviour
 {
-    public TileType tileType = TileType.idle;
+    public TileType tileType = TileType.grass;
 
+    // Tile의 Sprite
     [SerializeField]
     private SpriteRenderer tileRenderer;
+    [SerializeField]
+    private Sprite[] tileSprites;
+    [SerializeField]
+    private Sprite[] grassTileSprites;
 
     [SerializeField]
     private GameObject highlight;
 
 
 
-    public void SetTileColor(bool chooseColor)
+    public void SettingTile(TileType type)
     {
-        if (chooseColor) tileRenderer.color = new Color(0.164f, 0.271f, 0.274f);
-        else tileRenderer.color = new Color(0.164f, 0.204f, 0.274f);
+        SetTileSprite();
+    }
+
+    private void SetTileSprite()
+    {
+        // Tile Sprite 설정
+        if (tileType == TileType.grass) SetGrassTileSprite();
+        else tileRenderer.sprite = tileSprites[(int)tileType];
+    }
+
+    private void SetGrassTileSprite()
+    {
+        // 기본 Grass Sprite 랜덤으로 생성
+        tileRenderer.sprite = grassTileSprites[Random.Range(0, grassTileSprites.Length)];
     }
 
     private void OnMouseEnter()
     {
+        if (tileType == TileType.grass)
         highlight.SetActive(true);
     }
 
