a25d5e82ef3f0aa21d270a246f9d0b8d25af2b16
Medorpond <stcwh@naver.com>
2024-04-12 04:46:37 +0900

    Astar Pathfinder Complete
     - Astar Pathfinder now Works
     - Need attatching to Unit script

--- Assets/Scripts/A_star/MapMakerForAStarTest.cs
+++ Assets/Scripts/A_star/MapMakerForAStarTest.cs
@@ -19,6 +19,8 @@ namespace Astar
 
         public Node[,] NodeArray;
 
+        private List<Node> Path;
+
         private void Awake()
         {
             NodeArray = new Node[width, height];
@@ -26,45 +28,54 @@ namespace Astar
         private void Start()
         {
             GenerateGrid();
-            
+
         }
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                PathFinder pathfinder = new PathFinder(NodeArray, new Vector2Int(0, 0), new Vector2Int(3, 5), 5);
-                pathfinder.PathFinding();
-                pathfinder.OnDrawGizmos();
+                PathFinder pathfinder = new PathFinder(NodeArray, new Vector2Int(0, 0), new Vector2Int(0, 5), 5);
+                Path = pathfinder.PathFinding();
+                showPath();
             }
         }
 
         private void GenerateGrid()
         {
             bool isBlocked;
-            for (int x = 0; x < width * 10; x += 10)
+            for (int x = 0; x < width; x += 1)
             {
-                for (int y = 0; y < height * 10; y += 10)
+                for (int y = 0; y < height; y += 1)
                 {
                     // 타일 생성
                     MapTile spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity, gameObject.transform);
                     spawnedTile.name = $"Tile({x}, {y})";
 
-                    if ((x == 10 && y == 20) ||
-                        (x == 20 && y == 10) ||
-                        (x == 20 && y == 20) ||
-                        (x == 40 && y == 10) ||
-                        (x == 40 && y == 20) ||
-                        (x == 40 && y == 30) ||
-                        (x == 40 && y == 40) ||
-                        (x == 10 && y == 40) ||
-                        (x == 20 && y == 40) ||
-                        (x == 30 && y == 40) ||
-                        (x == 40 && y == 40)) { isBlocked = true; }
+                    if ((x == 0 && y == 4) ||
+                        (x == 0 && y == 2) ||
+                        (x == 1 && y == 2) ||
+                        (x == 2 && y == 1) ||
+                        (x == 2 && y == 2) ||
+                        (x == 4 && y == 1) ||
+                        (x == 4 && y == 2) ||
+                        (x == 4 && y == 3) ||
+                        (x == 4 && y == 4) ||
+                        (x == 1 && y == 4) ||
+                        (x == 2 && y == 4) ||
+                        (x == 3 && y == 4) ||
+                        (x == 4 && y == 4)) { isBlocked = true; }
                     else { isBlocked = false; }
                     // 노드 추가
-                    NodeArray[x / 10, y / 10] = new Node(isBlocked, x, y);
+                    NodeArray[x, y] = new Node(isBlocked, x, y);
                 }
+            }
+        }
+        void showPath()
+        {
+            for (int i = 0; i < Path.Count; i++)
+            {
+                Debug.Log(i + "번째 경로: " + Path[i].x + ", " + Path[i].y);
             }
         }
     }
--- Assets/Scripts/A_star/PathFinder.cs
+++ Assets/Scripts/A_star/PathFinder.cs
@@ -15,8 +15,8 @@ namespace Astar
 
             StartNode = _NodeArray[_startpoint.x, _startpoint.y];
             TargetNode = _NodeArray[_destination.x, _destination.y];
-            restrictBottom = _NodeArray[0, 0];
-            restrictTop = _NodeArray[15, 15];
+            restrictBottom = new Vector2(_NodeArray[0, 0].x, _NodeArray[0, 0].y); // Start of Map, BottomLeft
+            restrictTop = new Vector2(_NodeArray[15, 15].x, _NodeArray[15, 15].y); // End of Map, TopRight
 
             StartNode.D = 0;
 
@@ -29,15 +29,14 @@ namespace Astar
         #region Parameter
         Node[,] NodeArray;
         Node StartNode, TargetNode, CurrentNode;
-        Node restrictBottom, restrictTop;
+        Vector2 restrictBottom, restrictTop;
         int movePoint;
-        int scale;
         List<Node> OpenList, ClosedList;
 
         public List<Node> Path;
         #endregion
 
-        public void PathFinding()
+        public List<Node> PathFinding()
         {
             while (OpenList.Count > 0)
             {
@@ -61,37 +60,39 @@ namespace Astar
                     Path.Reverse();
                 }
 
-                Scan(CurrentNode.x, CurrentNode.y + 10); // 상
-                Scan(CurrentNode.x, CurrentNode.y - 10); // 하
-                Scan(CurrentNode.x - 10, CurrentNode.y); // 좌
-                Scan(CurrentNode.x + 10, CurrentNode.y); // 우
-                Scan(CurrentNode.x + 10, CurrentNode.y + 10); // 대각 우상
-                Scan(CurrentNode.x - 10, CurrentNode.y + 10); // 대각 좌상
-                Scan(CurrentNode.x + 10, CurrentNode.y - 10); // 대각 우하
-                Scan(CurrentNode.x - 10, CurrentNode.y - 10); // 대각 좌하
+                Scan(CurrentNode.x, CurrentNode.y + 1); // 상
+                Scan(CurrentNode.x, CurrentNode.y - 1); // 하
+                Scan(CurrentNode.x - 1, CurrentNode.y); // 좌
+                Scan(CurrentNode.x + 1, CurrentNode.y); // 우
+                /*
+                Scan(CurrentNode.x + 1, CurrentNode.y + 1); // 대각 우상
+                Scan(CurrentNode.x - 1, CurrentNode.y + 1); // 대각 좌상
+                Scan(CurrentNode.x + 1, CurrentNode.y - 1); // 대각 우하
+                Scan(CurrentNode.x - 1, CurrentNode.y - 1); // 대각 좌하
+                대각선 비활성... */
+                Debug.Log("실행!");
             }
+            return Path;
         }
 
         void Scan(int _scanX, int _scanY)
         {
-            if (_scanX <= 0 || _scanY <= 0)
-            {
-                return;
-            }
-            Node OnScanNode = NodeArray[_scanX/10, _scanY/10];
-
-            if (OnScanNode.isBlocked || ClosedList.Contains(OnScanNode))
-            { return; }// 탐색 대상이 벽이거나 이미 경로에 있으면 중단
-
-            if (NodeArray[CurrentNode.x/10, _scanY / 10].isBlocked ||
-                NodeArray[_scanX / 10, CurrentNode.y / 10].isBlocked)
-            { return; }// 대각선 이동 중 코너를 가로지르게 되는 경우 중단
-
             if (_scanX < restrictBottom.x || _scanX > restrictTop.x ||
                 _scanY < restrictBottom.y || _scanY > restrictTop.y)
             { return; }// 맵 경계 외부를 탐색할 경우 중단
 
-            int moveCost = CurrentNode.D + (CurrentNode.x == _scanX || CurrentNode.y == _scanY ? 10 : 14);
+            Node OnScanNode = NodeArray[_scanX, _scanY];
+
+            if (OnScanNode.isBlocked || ClosedList.Contains(OnScanNode))
+            { return; }// 탐색 대상이 벽이거나 이미 경로에 있으면 중단
+
+            if (NodeArray[CurrentNode.x, _scanY].isBlocked ||
+                NodeArray[_scanX, CurrentNode.y].isBlocked)
+            { return; }// 대각선 이동 중 코너를 가로지르게 되는 경우 중단
+
+
+
+            int moveCost = CurrentNode.D + 1; // 대각선을 허용할 경우 => (CurrentNode.x == _scanX || CurrentNode.y == _scanY ? 10 : 14);
 
             if (!OpenList.Contains(OnScanNode) || moveCost < OnScanNode.D)
             {
@@ -101,18 +102,6 @@ namespace Astar
 
                 OpenList.Add(OnScanNode);
             }
-        }
-
-        public void OnDrawGizmos()
-        {
-            if (Path.Count != 0)
-            {
-                for (int i = 0; i < Path.Count; i++)
-                {
-                    Debug.Log("Now: " + Path[i].x + ", " + Path[i].y);
-                }
-            }
-            else { Debug.Log("비었는데?"); }
         }
     }
 }
