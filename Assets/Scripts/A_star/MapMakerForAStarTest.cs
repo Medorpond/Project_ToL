dc736bccc2ec419fcfdbb605a749d64696ea7039
Medorpond <stcwh@naver.com>
2024-04-12 04:56:34 +0900

    Script Adjustment
     - Node script exist no more;
     - Node class is now with Pathfinder script

--- Assets/Scripts/A_star/MapMakerForAStarTest.cs
+++ Assets/Scripts/A_star/MapMakerForAStarTest.cs
@@ -1,82 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Astar;
 
-
-namespace Astar
+public class MapMakerForAStarTest : MonoBehaviour
 {
-    public class MapMakerForAStarTest : MonoBehaviour
+    [Header("Board Info")]
+    [SerializeField]
+    private int width;
+
+    [SerializeField]
+    private int height;
+
+    [SerializeField]
+    private int destX;
+
+    [SerializeField]
+    private int destY;
+
+    [SerializeField]
+    private MapTile tilePrefab;
+
+    public Node[,] NodeArray;
+
+    private List<Node> Path;
+
+    private void Awake()
     {
-        [Header("Board Info")]
-        [SerializeField]
-        private int width;
+        NodeArray = new Node[width, height];
+    }
+    private void Start()
+    {
+        GenerateGrid();
 
-        [SerializeField]
-        private int height;
+    }
 
-        [SerializeField]
-        private MapTile tilePrefab;
-
-        public Node[,] NodeArray;
-
-        private List<Node> Path;
-
-        private void Awake()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            NodeArray = new Node[width, height];
+            PathFinder pathfinder = new PathFinder(NodeArray, new Vector2Int(0, 0), new Vector2Int(destX, destY), 5);
+            Path = pathfinder.PathFinding();
+            showPath();
         }
-        private void Start()
-        {
-            GenerateGrid();
+    }
 
-        }
-
-        private void Update()
+    private void GenerateGrid()
+    {
+        bool isBlocked;
+        for (int x = 0; x < width; x += 1)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            for (int y = 0; y < height; y += 1)
             {
-                PathFinder pathfinder = new PathFinder(NodeArray, new Vector2Int(0, 0), new Vector2Int(0, 5), 5);
-                Path = pathfinder.PathFinding();
-                showPath();
+                // 타일 생성
+                MapTile spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity, gameObject.transform);
+                spawnedTile.name = $"Tile({x}, {y})";
+
+                if ((x == 0 && y == 4) ||
+                    (x == 0 && y == 2) ||
+                    (x == 1 && y == 2) ||
+                    (x == 2 && y == 1) ||
+                    (x == 2 && y == 2) ||
+                    (x == 4 && y == 1) ||
+                    (x == 4 && y == 2) ||
+                    (x == 4 && y == 3) ||
+                    (x == 4 && y == 4) ||
+                    (x == 1 && y == 4) ||
+                    (x == 2 && y == 4) ||
+                    (x == 3 && y == 4) ||
+                    (x == 4 && y == 4)) { isBlocked = true; }
+                else { isBlocked = false; }
+                // 노드 추가
+                NodeArray[x, y] = new Node(isBlocked, x, y);
             }
         }
-
-        private void GenerateGrid()
+    }
+    void showPath()
+    {
+        for (int i = 0; i < Path.Count; i++)
         {
-            bool isBlocked;
-            for (int x = 0; x < width; x += 1)
-            {
-                for (int y = 0; y < height; y += 1)
-                {
-                    // 타일 생성
-                    MapTile spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity, gameObject.transform);
-                    spawnedTile.name = $"Tile({x}, {y})";
-
-                    if ((x == 0 && y == 4) ||
-                        (x == 0 && y == 2) ||
-                        (x == 1 && y == 2) ||
-                        (x == 2 && y == 1) ||
-                        (x == 2 && y == 2) ||
-                        (x == 4 && y == 1) ||
-                        (x == 4 && y == 2) ||
-                        (x == 4 && y == 3) ||
-                        (x == 4 && y == 4) ||
-                        (x == 1 && y == 4) ||
-                        (x == 2 && y == 4) ||
-                        (x == 3 && y == 4) ||
-                        (x == 4 && y == 4)) { isBlocked = true; }
-                    else { isBlocked = false; }
-                    // 노드 추가
-                    NodeArray[x, y] = new Node(isBlocked, x, y);
-                }
-            }
-        }
-        void showPath()
-        {
-            for (int i = 0; i < Path.Count; i++)
-            {
-                Debug.Log(i + "번째 경로: " + Path[i].x + ", " + Path[i].y);
-            }
+            Debug.Log(i + "번째 경로: " + Path[i].x + ", " + Path[i].y);
         }
     }
 }
--- Assets/Scripts/A_star/PathFinder.cs
+++ Assets/Scripts/A_star/PathFinder.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 
 namespace Astar
 {
+    public class Node
+    {
+        public Node(bool _isBlocked, int _x, int _y) { isBlocked = _isBlocked; x = _x; y = _y; }
+
+        // D for distance(from Origin), H for Heuristic value, W for total Weigh(D + H)
+        public bool isBlocked;
+        public Node ParantNode;
+        public int x, y, D, H;
+        public int W { get { return D + H; } }
+    }
+
+
     public class PathFinder
     {
         // scale은 10 (14 for Diagnal)으로 임의설정됨. 빠른 연산을 위해 int형 scale 필요.
@@ -70,7 +82,6 @@ namespace Astar
                 Scan(CurrentNode.x + 1, CurrentNode.y - 1); // 대각 우하
                 Scan(CurrentNode.x - 1, CurrentNode.y - 1); // 대각 좌하
                 대각선 비활성... */
-                Debug.Log("실행!");
             }
             return Path;
         }
