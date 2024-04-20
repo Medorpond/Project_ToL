8c808690fbfa0f60863277000b11230603d46319
POOZIM <wdg111@naver.com>
2024-04-20 19:10:08 +0900

    add comment CreateCharSelect.cs
    .

--- Assets/Scripts/TurnBasedGame/CreateCharSelect.cs
+++ Assets/Scripts/TurnBasedGame/CreateCharSelect.cs
@@ -10,37 +10,37 @@ using UnityEngine.UI;
 public class CreateCharSelect : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] CharBtn;
+    private GameObject[] CharBtn; // prefabs
     [SerializeField]
-    private GameObject DoneBtn;
-    Vector3 createPoint = new Vector3(0, 100, 0);
-    private int x = 0;
-    private int clickCount = 0;
-    List<GameObject> setButtons = new List<GameObject>();
-    List<GameObject> createdBtns = new List<GameObject>();
-    GameObject instantiatedDoneBtn;
+    private GameObject DoneBtn; // done button
+    Vector3 createPoint = new Vector3(0, 100, 0); // first spawn
+    private int x = 0; // + 100
+    private int clickCount = 0; // 3 ~ 7 char count
+    List<GameObject> setButtons = new List<GameObject>(); // for destroy
+    List<GameObject> createdBtns = new List<GameObject>(); // for reference
+    GameObject instantiatedDoneBtn; // for destroy
     void Start()
     {
-        Transform parentTransform = GameObject.Find("SelectChar").transform;
-        for(int i = 0; i < CharBtn.Length; i++)
+        Transform parentTransform = GameObject.Find("SelectChar").transform; // sub SelectChar
+        for(int i = 0; i < CharBtn.Length; i++) // create 1 level button(choose button)
         {
-            GameObject instantiatedBtn = Instantiate(CharBtn[i], Vector3.zero, Quaternion.identity, parentTransform);
-            instantiatedBtn.transform.localPosition = createPoint; // local location set
-            createPoint.x += 100;
-            setButtons.Add(instantiatedBtn);
+            GameObject instantiatedBtn = Instantiate(CharBtn[i], Vector3.zero, Quaternion.identity, parentTransform); // create 0, 0, 0 
+            instantiatedBtn.transform.localPosition = createPoint; // move to createPoint
+            createPoint.x += 100; // next + 100
+            setButtons.Add(instantiatedBtn); // setButtons add
 
-            Button button = instantiatedBtn.GetComponent<Button>();
-            if (button != null)
+            Button button = instantiatedBtn.GetComponent<Button>(); // button click
+            if (button != null) 
             {
-                int index = i;
+                int index = i; // ex) 1 = tanker, 2 = dealer, ...
                 button.onClick.AddListener(() => OnButtonClick(index));
             }
         }
-        createPoint = new Vector3(710, 175, 0);
-        instantiatedDoneBtn = Instantiate(DoneBtn, Vector3.zero, Quaternion.identity, parentTransform);
-        instantiatedDoneBtn.transform.localPosition = createPoint;
+        createPoint = new Vector3(710, 175, 0); // right-top
+        instantiatedDoneBtn = Instantiate(DoneBtn, Vector3.zero, Quaternion.identity, parentTransform); // create end button
+        instantiatedDoneBtn.transform.localPosition = createPoint; // move to craetePoint
 
-        Button Donebutton = instantiatedDoneBtn.GetComponent<Button>();
+        Button Donebutton = instantiatedDoneBtn.GetComponent<Button>(); // end button click
         if (Donebutton != null)
         {
             Donebutton.onClick.AddListener(onDoneButtonClick);
@@ -48,23 +48,23 @@ public class CreateCharSelect : MonoBehaviour
 
     }
 
-    void OnButtonClick(int index){
-            if(index == 0){
-            Transform parent = GameObject.Find("SelectChar").transform;
-            GameObject TankerButton = Instantiate(CharBtn[index], Vector3.zero, Quaternion.identity, parent);
-            TankerButton.transform.localPosition = new Vector3(x, 0, 0);
-            x += 100;
-            createdBtns.Add(TankerButton);
-            Button buttonComponent = TankerButton.GetComponent<Button>();
+    void OnButtonClick(int index){ // click 1-level button(choose)
+            if(index == 0){ // ex) 1 = tanker, 2 = dealer, ...
+            Transform parent = GameObject.Find("SelectChar").transform;       // sub SelectChar
+            GameObject TankerButton = Instantiate(CharBtn[index], Vector3.zero, Quaternion.identity, parent); // create 2-level button (tanker)
+            TankerButton.transform.localPosition = new Vector3(x, 0, 0);    // left-bottom
+            x += 100;   // next + 100
+            createdBtns.Add(TankerButton); // List add 
+            Button buttonComponent = TankerButton.GetComponent<Button>(); // click 2-level button (select)
             if (buttonComponent != null)
             {
                 buttonComponent.onClick.AddListener(() =>
                 {
-                    Debug.Log("TankerButton Clicked!");
+                    Debug.Log("TankerButton Clicked!"); // print log
                 });
             }
             }
-        else if (index == 1){
+        else if (index == 1){ // ex) 1 = tanker, 2 = dealer, ...
             Transform parent = GameObject.Find("SelectChar").transform;
             GameObject DealerButton = Instantiate(CharBtn[index], Vector3.zero, Quaternion.identity, parent);
             DealerButton.transform.localPosition = new Vector3(x, 0, 0);
@@ -94,24 +94,24 @@ public class CreateCharSelect : MonoBehaviour
                 });
             }
         }
-        clickCount++;
-        if (clickCount >= 7)
+        clickCount++;      // created 2-level button count
+        if (clickCount >= 7)    // why 7? size issue
         {
-            onDoneButtonClick();
+            onDoneButtonClick(); // auto end
         }
 
     }
 
-    void onDoneButtonClick(){
-        if (clickCount >= 3 ){
-            Destroy(instantiatedDoneBtn);
-            foreach(GameObject btn in setButtons)
+    void onDoneButtonClick(){ // click end 
+        if (clickCount >= 3 ){      // created at least 3
+            Destroy(instantiatedDoneBtn); // destroy end button
+            foreach(GameObject btn in setButtons) // destroy 1-level button
             {
                 Destroy(btn);
             }
             foreach(GameObject btn in createdBtns)
             {
-                btn.transform.localPosition += new Vector3(20f, 50f, 0f);
+                btn.transform.localPosition += new Vector3(20f, 50f, 0f); // move 2-level button
             }
         }
         
