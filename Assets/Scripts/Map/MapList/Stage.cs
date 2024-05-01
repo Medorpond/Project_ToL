997d2d5f4dfb570050b5fd4122b1225f69856f07
Medorpond <134402610+Medorpond@users.noreply.github.com>
2024-05-02 02:13:21 +0900

    -
    -

--- Assets/Scripts/Map/MapList/Stage.cs
+++ Assets/Scripts/Map/MapList/Stage.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NodeStruct;
 
 public abstract class Stage : MonoBehaviour
 {
-    protected string symType;
-    protected string mapType;
-    protected int mapSize;
-    
+    protected string stageType;
+    protected int stageSize;
+
+    protected Node[,] NodeArray; //PreDefine NodeArray for each Stage...
 
     public void Load()
     {
@@ -16,8 +17,25 @@ public abstract class Stage : MonoBehaviour
 
     protected abstract void Init(); // Map Initialize itself by this Method.
 
-    private void GenBuff()
+    protected virtual void GenBuff() // 밸런스 문제로 보류
     {
-
+        int numBuff; // Quantity of Buff or Debuff on Stage.
+        switch (stageSize)
+        {
+            case 3:
+                numBuff = 1;
+                break;
+            case 5:
+                numBuff = 2;
+                break;
+            case 7:
+                numBuff = 3;
+                break;
+            default:
+                Debug.Log("Wrong MapSize");
+                numBuff = 1;
+                break;
+        }// Set numBuff by stageSize.
+        //Init Buffs under...
     }
 }
