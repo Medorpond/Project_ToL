e6b2c0ecd6758d56d8d15fe9ce622e9c87f371fc
jyjiwoo <thswldn0317@kau.kr>
2024-05-09 14:04:54 +0900

    camera zoom in out

--- Assets/Scripts/TurnBasedGame/CameraManager.cs
+++ Assets/Scripts/TurnBasedGame/CameraManager.cs
@@ -13,5 +13,17 @@ public class CameraManager : MonoBehaviour
     private void Update()
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float newSize = Camera.main.orthographicSize - scroll * zoomSpeed;
+        newSize = Mathf.Clamp(newSize, minZoom, maxZoom);
+
+        // 마우스 포인터의 현재 위치를 가져옴
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 현재 카메라의 위치와 마우스 포인터 사이의 거리 계산
+        float distanceToMouse = Vector2.Distance(transform.position, mousePos);
+
+        // 카메라를 마우스 포인터로 이동하고 줌 인/아웃
+        transform.position = Vector3.MoveTowards(transform.position, mousePos, scroll * zoomSpeed * distanceToMouse);
+        Camera.main.orthographicSize = newSize;
     }
 }
